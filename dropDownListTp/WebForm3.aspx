892793a6fb5764382072993ac48ca318793438d0
elhajuojy <souirimehdi311@gmail.com>
2022-04-15 13:03:45 +0000

    working with UploadFile

---
+++ dropDownListTp/WebForm3.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm3.aspx.cs" Inherits="dropDownListTp.WebForm3" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:FileUpload ID="FileUpload1" runat="server"/>&nbsp;
+            <asp:Button  ID="btnUpload" runat="server" OnClick="btnUpload_Click" Text="Upload"/>
+            <br />
+            <asp:Label ID="lblMessage" runat="server" Font-bold="true" > </asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ dropDownListTp/WebForm3.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace dropDownListTp
+{
+    public partial class WebForm3 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnUpload_Click(object sender, EventArgs e)
+        {
+            if (FileUpload1.HasFile)
+            {
+                string FileExtension = System.IO.Path.GetExtension(FileUpload1.FileName);
+                if(FileExtension.ToLower()!=".pdf")
+                {
+                    lblMessage.Text = "Seulement les fichiers avec l'extension .pdf sont autorises";
+                    lblMessage.ForeColor = System.Drawing.Color.Red;
+                }
+                else
+                {
+                    int fileSize = FileUpload1.PostedFile.ContentLength;
+                    if(fileSize>2097152) // 2 M
+                    {
+                        lblMessage.Text = "la taille de votre fichier" + fileSize.ToString();
+                        lblMessage.ForeColor = System.Drawing.Color.Red;
+                    }
+                    else
+                    {
+                        FileUpload1.SaveAs(Server.MapPath("~/Uploads/") + FileUpload1.FileName);
+                        lblMessage.Text = "Fichier telecharge avec succes";
+                        lblMessage.ForeColor = System.Drawing.Color.Green;
+                    }
+                }
+            }
+            else
+            {
+                lblMessage.Text = "Vous dezes selectionner un ficher";
+                lblMessage.ForeColor = System.Drawing.Color.Red;
+            }
+        }
+    }
+}
